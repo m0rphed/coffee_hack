1d211849f4d92fdde2986fa7fdf935c3b6b7da92
Евгения Галеева <zenagaleeva1315@gmail.com>
2024-12-15 17:47:03 +0300

    Созданы и размечены страницы

--- frontend-baggins/src/App.js
+++ frontend-baggins/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainPage from "./pages/mainPage";
+import AnalisPage from './pages/analisPage';
+import UsersPage from './pages/usersPage';
+import OneUserPage from './pages/oneUserPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />}></Route>
+        <Route path='/analis' element={<AnalisPage />}></Route>
+        <Route path='/users' element={<UsersPage />}></Route>
+        <Route path='/oneuser' element={<OneUserPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend-baggins/src/pages/usersPage.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Pane, Tablist, Tab, Paragraph } from 'evergreen-ui'
+
+const UsersPage = () => {
+    const [selectedIndex, setSelectedIndex] = useState(0)
+    const [tabs] = useState(['График 1', 'График 2', 'График 3'])
+    return (
+        <div className="UsersPage">
+            <h1>
+                Общие тренды гостей
+            </h1>
+            <Pane height={120}>
+            <Tablist marginBottom={16} flexBasis={240} marginRight={24}>
+                {tabs.map((tab, index) => (
+                <Tab
+                    aria-controls={`panel-${tab}`}
+                    isSelected={index === selectedIndex}
+                    key={tab}
+                    onSelect={() => setSelectedIndex(index)}
+                >
+                    {tab}
+                </Tab>
+                ))}
+            </Tablist>
+            <Pane padding={16} background="tint1" flex="1">
+                {tabs.map((tab, index) => (
+                <Pane
+                    aria-labelledby={tab}
+                    aria-hidden={index !== selectedIndex}
+                    display={index === selectedIndex ? 'block' : 'none'}
+                    key={tab}
+                    role="tabpanel"
+                >
+                    <Paragraph>Описание и сам {tab}</Paragraph>
+                </Pane>
+                ))}
+            </Pane>
+            </Pane>
+        </div>
+    )
+}
+
+export default UsersPage;
